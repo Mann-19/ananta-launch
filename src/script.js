68a5275b41902f905af6733f9d90fd02d13c8d5a
Mann-19 <tkp2510@gmail.com>
2024-02-07 18:50:58 +0530

    Launch Site Finished

---
+++ src/script.js
@@ -0,0 +1,27 @@
+const launchBtn = document.getElementById('launch-btn');
+const rocket = document.getElementById('rocket');
+const audio = new Audio('/public/audio/we_are_safemoon.mp3');
+
+function startTimer() {
+  launchBtn.style.display = 'none';
+  let seconds = 1;
+  audio.play();
+  
+  const timerInterval = setInterval(function () {
+    document.getElementById('timer').innerHTML = seconds + ' seconds remaining';
+    
+    if (seconds === 0) {
+          clearInterval(timerInterval);
+          document.getElementById('timer').innerHTML = "Let's Go!!!";
+          rocket.style.animationName = 'fly';
+          rocket.style.animationDuration = '4s';
+      } else {
+          seconds--;
+      }
+
+      rocket.addEventListener('animationend', () => {
+        rocket.style.bottom = "200vh";
+        window.location = 'https://www.anantagsfcu.in/';
+      })
+  }, 1000);
+}
